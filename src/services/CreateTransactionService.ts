f9879d23f5256f3d0c77ae967fc32b743cb07409
Matheus Teixeira <mathcardosoteixeira@gmail.com>
2020-11-04 15:46:12 -0300

    feat: add list and create transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,16 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Balance {
+  total: number;
+}
+
+interface RequestTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +18,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute(
+    { title, value, type }: RequestTransaction,
+    { total }: Balance,
+  ): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type is not valid');
+    }
+
+    if (type === 'outcome' && total < value) {
+      throw Error('This value exceeds the total cash');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
